3bb224433002d2b5d76cedf23233de7a0aaeffa4
dgrondona <draven.grondona@gmail.com>
2026-02-15 13:52:51 -0800

    Lab 02

---
+++ Lab Assignments/Lab_2/dgrondona.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+int* merge(int* left, int* right, int leftHalf, int rightHalf) {
+
+    int size = leftHalf + rightHalf;
+    int* result = new int[size];
+
+    int a = 0;
+    int b = 0;
+
+    // FIX ME: does not work for arrays of different sizes
+    for (int i = 0; i < (size); i++) {
+
+        // std::cerr << "a: " << a << ",  b: " << b << ",  size: " << size << std::endl;
+
+        if (b >= rightHalf || (a < leftHalf && left[a] < right[b])) {
+
+            // std::cerr << "merging left: " << left[a] << std::endl;
+
+            result[i] = left[a++];
+
+        } else {
+
+            // std::cerr << "merging right: " << right[b] << std::endl;
+
+            result[i] = right[b++];
+
+        }
+
+    }
+
+    return result;
+
+}
+
+// Mergesort Function
+int* mergesort(int* input, int size) {
+
+    int* result;
+
+    if (size <= 1) {
+        result = new int[1];
+        result[0] = input[0];
+        return result;
+    }
+
+    // int division automatically truncated
+    int leftHalf = size / 2;
+    int rightHalf = size - leftHalf;
+
+    int* left = mergesort(input, leftHalf);
+    int* right = mergesort(input + leftHalf, rightHalf);
+
+    result = merge(left, right, leftHalf, rightHalf);
+
+    delete[] left;
+    delete[] right;
+
+    return result;
+
+}
+
+int main(int argc, char **argv) {
+
+    // Variables to get our inputted size and contents
+    int* input;
+    int size;
+
+    // Get the size from the first input and make an array of that size
+    std::cin >> size;
+    // std::cout << "--------" << size << ", " << (size % 2) << std::endl;
+    input = new int[size];
+
+    // Fill the array with elements from the input
+    for (int i = 0; i < size; i++) {
+
+        std::cin >> input[i];
+
+    }
+    
+    int* result = mergesort(input, size);
+
+    for (int i = 0; i < size; i++) {
+
+        std::cout << result[i] << ";";
+
+    }
+
+    std::cout << std::endl;
+
+    // Free allocated memory
+    delete[] result;
+    delete[] input;
+
+}
