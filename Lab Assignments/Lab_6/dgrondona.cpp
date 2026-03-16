e623f4544cb24c58374ed38552840990b51438cc
dgrondona <draven.grondona@gmail.com>
2026-03-16 11:39:59 -0700

    filled some of the Hash Table functions

--- Lab Assignments/Lab_6/dgrondona.cpp
+++ Lab Assignments/Lab_6/dgrondona.cpp
@@ -6,16 +6,62 @@ using namespace std;
 
 class HashTable {
 private:
+
     int tableSize;
     std::vector<list<int>> table;
 
     int hashFunction(int key);
+
 public:
-    HashTable(int size);
 
-    void insert(int key);
-    bool search(int key);
-    void remove(int key);
+    // Constructor.
+    HashTable(int size) {
+        tableSize = size;
+        table.resize(size);
+    }
 
-    void display();
+    // Insert key into table; i2 implies "Insert key 2 into the table".
+    void insert(int key) {
+        int i = hashFunction(key);
+        table[i].push_back(key);
+    }
+
+    // Search table for key.
+    bool search(int key) {
+        int i = hashFunction(key);
+
+        for (int j = 0; j < table[i]; j++) {
+            if (j == key) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Delete key from the table; d2 implies “Delete key 2 from the table".
+    void remove(int key) {
+        int i = hashFunction(key);
+        table[i].remove(key);
+    }
+
+    // Hash function: h(k) = k % m (where k is the key and m is the size of the table).
+    int hashFunction(int key) {
+        return key % tableSize;
+    }
+
+    void display() {
+        return;
+    }
+
 };
+
+int main (int argc, char **argv) {
+
+    HashTable ht(10);
+
+    ht.insert(10);
+    ht.insert(5);
+    ht.insert(7);
+
+}
