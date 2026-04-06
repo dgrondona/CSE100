1445cab9aad141de2abfd2478efdfedf4ae08e66
dgrondona <draven.grondona@gmail.com>
2026-04-05 19:49:17 -0700

    added helpers for the different traversals

--- Lab Assignments/Lab_7/dgrondona.cpp
+++ Lab Assignments/Lab_7/dgrondona.cpp
@@ -32,3 +32,35 @@ Node* insert(Node* root, int key) {
     return root;
 
 }
+
+void inOrder(Node* root) {
+
+    if (!root) return;
+
+    inOrder(root->left);
+    std::cout << root->key << std::endl;
+
+    inOrder(root->right);
+}
+
+void preOrder(Node* root) {
+
+    if (!root) return;
+
+    std::cout << root->key << std::endl;
+
+    preOrder(root->left);
+    preOrder(root->right);
+
+}
+
+void postOrder(Node* root) {
+
+    if (!root) return;
+
+    postOrder(root->left);
+    postOrder(root->right);
+
+    std::cout << root->key << std::endl;
+
+}
